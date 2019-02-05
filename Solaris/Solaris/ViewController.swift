97055091cb176bad301e164296cac219512c1043
djkw <thesunjumper@gmail.com>
2019-02-05 20:58:39 +0100

    Create view controlers

---
+++ Solaris/Solaris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Solaris
+//
+//  Created by Dawid Jankowski on 05/02/2019.
+//  Copyright © 2019 Dawid Jankowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
